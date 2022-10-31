47b28dbbe106fb642a5f4c06e77013475a1f3d62
Vitor Marques <civitao011@gmail.com>
2022-10-31 10:24:49 -0300

    fix

--- src/components/GamesCard.tsx
+++ src/components/GamesCard.tsx
@@ -9,7 +9,7 @@ export function GamesCard({...props}: GamesCardProps) {
   return(
     <div className="flex z-10 flex-col h-[480px] w-[186px] ">
     <img className="w-64  mix-blend-normal h-48 object-fill " src={props.image}></img>  
-    <p className="text-[13px]  leading-[26px] font-light mt-6 text-white w-full">{props.description}</p>
+    <p className="text-[13px]  leading-[26px] font-light mt-6 md: text-white w-full">{props.description}</p>
   </div>
     
   )
